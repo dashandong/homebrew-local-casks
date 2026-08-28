56fc9d58b508bd65a1d3681ebaad2fb070d78309
dashandong <ddsh0205@gmail.com>
2026-08-28 20:56:38 +0000

    eudic 2026.07.28

--- Casks/eudic.rb
+++ Casks/eudic.rb
@@ -1,6 +1,6 @@
 cask "eudic" do
-  version "2026.07.21"
-  sha256 "c7b4981233e368a699528765bc7daec6461433b7c225402777e5834cf0088da5"
+  version "2026.07.28"
+  sha256 "b1ae348b7ba1845243395b07e91f95968c7dac1b55a3837c9b5ce2dfd3a39f24"
 
   url "https://static.eudic.net/pkg/eudicmac.dmg?v=#{version.dots_to_hyphens}",
       verified:   "static.frdic.com/pkg/",
